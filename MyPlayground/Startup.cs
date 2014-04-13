19ece2449025fd494746e9c49d7a47243f6f1855
adiuvo <conviva@inbox.com>
2014-04-12 21:08:14 -0700

    Just a place to play

---
+++ MyPlayground/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyPlayground.Startup))]
+namespace MyPlayground
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
