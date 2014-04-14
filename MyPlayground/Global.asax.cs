2230f59a2e623c2b709c7e6a164dad9a2e9680c5
Robert Innes <Robert.Innes@cableone.biz>
2014-04-14 12:44:08 -0700

    Added Bootstrap 3 templates

--- MyPlayground/Global.asax.cs
+++ MyPlayground/Global.asax.cs
@@ -18,6 +18,7 @@ namespace MyPlayground
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BootstrapEditorTemplatesConfig.RegisterBundles();
         }
     }
 }
