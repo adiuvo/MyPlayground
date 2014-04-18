e782d4664efebdce2b4de69b68cabcd6eb557b28
adiuvo <conviva@inbox.com>
2014-04-18 02:40:32 -0700

    Cleaned up logging code, more left to do.

--- MyPlayground/Plumbing/Interceptors/LogAspect.cs
+++ MyPlayground/Plumbing/Interceptors/LogAspect.cs
@@ -9,23 +9,29 @@
 
 namespace MyPlayground.Plumbing.Interceptors
 {
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+
     using Castle.Core.Logging;
     using Castle.DynamicProxy;
 
     /// <summary>
-    /// TODO The log aspect.
+    ///     TODO The log aspect.
     /// </summary>
     public class LogAspect : IInterceptor
     {
         #region Public Properties
 
         /// <summary>
-        /// Gets or sets the logger.
+        ///     Gets or sets the logger.
         /// </summary>
         public ILogger Logger { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether logging enabled.
+        ///     Gets or sets a value indicating whether logging enabled.
         /// </summary>
         public bool LoggingEnabled { get; set; }
 
@@ -33,27 +39,81 @@ namespace MyPlayground.Plumbing.Interceptors
 
         #region Public Methods and Operators
 
+        public static string CreateInvocationLogString(IInvocation invocation)
+        {
+            var sb = new StringBuilder(100);
+            sb.AppendFormat("Called: {0}.{1}(", invocation.TargetType.Name, invocation.Method.Name);
+            foreach (object argument in invocation.Arguments)
+            {
+                String argumentDescription = argument == null ? "null" : DumpObject(argument);
+                sb.Append(argumentDescription).Append(",");
+            }
+            if (invocation.Arguments.Any())
+            {
+                sb.Length--;
+            }
+            sb.Append(")");
+            return sb.ToString();
+        }
+
         /// <summary>
-        /// The intercept.
+        ///     The intercept.
         /// </summary>
         /// <param name="invocation">
-        /// The invocation.
+        ///     The invocation.
         /// </param>
         public void Intercept(IInvocation invocation)
         {
-            this.Logger.Info(
-                string.Format("Method Called -> {0}::{1}", invocation.TargetType.Name, invocation.Method.Name));
+            this.Logger.InfoFormat("{0}::{1}({2})", invocation.TargetType.Name, invocation.Method.Name, string.Join(", ", DumpObject(invocation.Arguments)));
+
             try
             {
                 invocation.Proceed();
             }
             finally
             {
-                this.Logger.Info(
-                    string.Format("Method returned -> {0}::{1}", invocation.TargetType.Name, invocation.Method.Name));
+                this.Logger.InfoFormat("Method returned -> {0}::{1}", invocation.TargetType.Name, invocation.ReturnValue);
             }
         }
 
+        #endregion
+
+        #region Methods
+
+        private static string DumpObject(object argument)
+        {
+            string value;
+
+            Type objtype = argument.GetType();
+            if (objtype == typeof(string) || objtype.IsPrimitive || !objtype.IsClass)
+            {
+                if (objtype.IsClass)
+                {
+                    value = SerializeObject(argument);
+                }
+                else
+                {
+                    value = objtype.ToString();
+                }
+            }
+            else
+            {
+                value = SerializeObject(argument);
+            }
+
+            return value;
+        }
+
+        public static string SerializeObject(object toSerialize)
+        {
+            if (toSerialize == null) return string.Empty;
+
+            var xmlSerializer = new XmlSerializer(toSerialize.GetType());
+            var textWriter = new StringWriter();
+
+            xmlSerializer.Serialize(textWriter, toSerialize);
+            return textWriter.ToString();
+        }
         #endregion
     }
 }
